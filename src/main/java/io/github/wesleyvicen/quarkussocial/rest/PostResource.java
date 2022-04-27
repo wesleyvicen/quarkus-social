a6b95e9ac12a0acba0a8fd5cbbac1bb86fc2738b
Wesley Vicente <wesley1535@hotmail.com>
2022-04-27 16:16:44 -0300

    Criando o Resource de Posts e os endpoints

---
+++ src/main/java/io/github/wesleyvicen/quarkussocial/rest/PostResource.java
@@ -0,0 +1,20 @@
+package io.github.wesleyvicen.quarkussocial.rest;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/users/{userId}/posts")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class PostResource {
+
+    @POST
+    public Response savePost(){
+        return Response.status(Response.Status.CREATED).build();
+    }
+    @GET
+    public Response listPosts(){
+        return Response.ok().build();
+    }
+}
