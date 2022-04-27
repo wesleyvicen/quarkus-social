c493e356d663b7cf12b9a0fbd774537f39c8f6cc
Wesley Vicente <wesley1535@hotmail.com>
2022-04-27 11:40:02 -0300

    Retornando um objeto de erro padrão no POST

--- src/main/java/io/github/wesleyvicen/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/wesleyvicen/quarkussocial/rest/UserResource.java
@@ -4,6 +4,7 @@ package io.github.wesleyvicen.quarkussocial.rest;
 import io.github.wesleyvicen.quarkussocial.domain.model.User;
 import io.github.wesleyvicen.quarkussocial.domain.repository.UserRepository;
 import io.github.wesleyvicen.quarkussocial.rest.dto.CreateUserRequest;
+import io.github.wesleyvicen.quarkussocial.rest.dto.ResponseError;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 
 import javax.inject.Inject;
@@ -35,9 +36,9 @@ public class UserResource {
 
         Set<ConstraintViolation<CreateUserRequest>> violations = validator.validate(userRequest);
         if(!violations.isEmpty()){
-            ConstraintViolation<CreateUserRequest> erro = violations.stream().findAny().get();
-            String errorMessage = erro.getMessage();
-            return Response.status(400).entity(errorMessage).build();
+
+            ResponseError responseError = ResponseError.createFromValidation(violations);
+            return Response.status(400).entity(responseError).build();
         }
 
         User user = new User();
