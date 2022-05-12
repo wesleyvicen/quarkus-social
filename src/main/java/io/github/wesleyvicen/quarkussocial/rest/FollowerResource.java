cb8bf317ac601b4a866676a780e7154e878f55bd
Wesley Vicente <wesley1535@hotmail.com>
2022-05-11 21:50:12 -0300

    Implementando o método de seguir um usuário

--- src/main/java/io/github/wesleyvicen/quarkussocial/rest/FollowerResource.java
+++ src/main/java/io/github/wesleyvicen/quarkussocial/rest/FollowerResource.java
@@ -1,13 +1,14 @@
 package io.github.wesleyvicen.quarkussocial.rest;
 
+import io.github.wesleyvicen.quarkussocial.domain.model.Follower;
 import io.github.wesleyvicen.quarkussocial.domain.repository.FollowerRepository;
 import io.github.wesleyvicen.quarkussocial.domain.repository.UserRepository;
+import io.github.wesleyvicen.quarkussocial.rest.dto.FollowerRequest;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/users/{userId}/followers")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -20,5 +21,21 @@ public class FollowerResource {
     public FollowerResource (FollowerRepository repository, UserRepository userRepository){
         this.repository = repository;
         this.userRepository = userRepository;
+    }
+
+    @PUT
+    public Response followersUser(@PathParam("userId") Long userId, FollowerRequest request){
+       var user = userRepository.findById(userId);
+       if(user == null){
+           return Response.status(Response.Status.NOT_FOUND).build();
+       }
+       var follower = userRepository.findById(request.getFollowerId());
+       var entity = new Follower();
+       entity.setUser(user);
+       entity.setFallower(follower);
+
+       repository.persist(entity);
+
+       return Response.status(Response.Status.NO_CONTENT).build();
     }
 }
